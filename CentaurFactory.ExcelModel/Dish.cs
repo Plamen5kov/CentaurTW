946cf2ef5b5903a1cd3cb8f10dd3a2026b757f1a
Yoanna Mareva <jo.thirdeyevision@gmail.com>
2014-09-01 18:07:28 +0300

    Deleted unnecessery files
    Deleted unnecessary files

---
+++ CentaurFactory.ExcelModel/Dish.cs
@@ -0,0 +1,29 @@
+﻿namespace CentaurFactory.Data
+{
+    using MongoDB.Bson;
+    using System;
+
+    public class Dish
+    {
+        ObjectId Id;
+        public string Name { get; set; }
+        private decimal price;
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                if (value == null || value <= 0)
+                {
+                    throw new ArgumentNullException("Price cannot be null or less than zero.");
+                }
+                this.price = value;
+            }
+        }
+
+    }
+}
---
+++ CentaurFactory.MySqlProvider/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentaurFactory.MySqlProvider
+{
+    using System.Data.Entity;
+
+    using CentaurFactory.Model;
+
+   public class DataContext : DBContext
+    {
+    }
+}
