15802a1b9ab93b8edc0cd6f9ddaa7cc934d47469
themagicis <themagicis777@gmail.com>
2014-09-02 23:04:26 +0300

    added menu

--- CentaurFactory.ConsoleClient/EntryPoint.cs
+++ CentaurFactory.ConsoleClient/EntryPoint.cs
@@ -29,11 +29,78 @@
     using Telerik.OpenAccess;
     using MySqlPRovider;
     using System.IO;
+using System.Collections;
+using CentaurFactory.Model;
 
     public class EntryPoint
     {
+        public static void Option1()
+        {
+            Console.WriteLine("Config data saved to Mongo DB");
+        }
+
+        public static void Option2()
+        {
+            Console.WriteLine("Confog data read from Mongo DB");
+            Console.WriteLine("Confog data written into MSSQL");
+        }
+
+        public static void Option3()
+        {
+            Console.WriteLine("Data loaded from Excel 2003");
+            Console.WriteLine("Data written into MSSQL");
+        }
+
+        public static void Option4()
+        {
+            Console.WriteLine("Data loaded from XML");
+            Console.WriteLine("Data written into MSSQL");
+        }
+
+        public static void Option5()
+        {
+            Console.WriteLine("Confog data read from Mongo DB");
+            Console.WriteLine("Confog data written into MSSQL");
+        }
+
+        public static void Option6()
+        {
+            Console.WriteLine("Data loaded from MSSQL");
+            Console.WriteLine("Data written in MySQL");
+            Console.WriteLine("Data exported to JSON");
+        }
+
+        public static void Option7()
+        {
+            Console.WriteLine("Data loaded from SQLite");
+            Console.WriteLine("Data loaded from MySQL");
+            Console.WriteLine("Data exported to XLSX");
+        }
+
+        public static void Option8()
+        {
+            Console.WriteLine("PDF file exported");
+        }
+
+        public static void Option9()
+        {
+            Console.WriteLine("XML file exported.");
+        }
+
         public static void Main()
         {
+            Menu menu = new Menu();
+            menu.AddItem("1", "Init MongoDB", Option1);
+            menu.AddItem("2", "TransferData from MongoDb to MSSQL", Option2);
+            menu.AddItem("3", "TransferData from Zipped Excel 2003 (xls) to MSSQL", Option3);
+            menu.AddItem("4", "TransferData from XML to MSSQL and MongoDB", Option4);
+            menu.AddItem("5", "Create report to MySQL and write JSON", Option5);
+            menu.AddItem("6", "Export data from SQLite and MySQL to Excel 2007(xlsx)", Option6);
+            menu.AddItem("7", "Generate PDF report", Option7);
+            menu.AddItem("8", "Generate XML report", Option8);
+            menu.AddEndItem("9", "Exit");
+            menu.Start();
+
             /* To be able to run this code you need first to download mongodb server local on your computer.
                this is the link for windows 64 
                https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-2.6.4.zip .
@@ -44,8 +111,8 @@
             //var mongoClient = new MongoClient("mongodb://192.168.0.100/");
             //var mongoServer = mongoClient.GetServer();
             //var centaurRestaurantDb = mongoServer.GetDatabase("CentaurRestaurantDb");
-            var mongoProvider = new MongoProvider(ConfigurationManager.AppSettings["mongoDB"], "centaur_restaurant_db");
-            var mongoRepo = new MongoRepository(mongoProvider);
+            //var mongoProvider = new MongoProvider(ConfigurationManager.AppSettings["mongoDB"], "centaur_restaurant_db");
+            //var mongoRepo = new MongoRepository(mongoProvider);
 
             // uncomment to load data to mongo db
             //mongoRepo.InitData();
@@ -73,7 +140,7 @@
             //    Console.WriteLine(item.ToString());
             //}
 
-            UpdateDatabase();
+            //UpdateDatabase();
 
             // ExtractZipFiles();
 
@@ -106,6 +173,7 @@
             {
                 var schemaHandler = context.GetSchemaHandler();
                 EnsureDB(schemaHandler);
+                //context.Add();
             }
         }
 
@@ -159,6 +227,15 @@
                     }
                 }
             }
+        }
+
+        public IEnumerable<DishReport> GetDishReports(IEnumerable<Sale> sales, IEnumerable<Ingredient> ingredients, DeliveryInfo delivery)
+        {
+            List<DishReport> reports = new List<DishReport>();
+
+
+
+            return reports;
         }
     }
 }
